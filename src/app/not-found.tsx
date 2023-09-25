1792305581dc445972c6276d0ec06f2a58d0b1e9
KhanhNguyen <nguyen.quoc.khanh.531@gmail.com>
2023-09-25 21:45:54 +0700

    use sass and create layout and not-found screen

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+// Note that `app/[locale]/[...rest]/page.tsx`
+// is necessary for this page to render.
+
+export default function NotFound() {
+
+  return (
+    <div>
+      <p>NotFoundPage</p>
+    </div>
+  );
+}
---
+++ src/components/FooterLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/layout.module.scss'
+
+const FooterLayout = () => {
+  return (
+    <div className={styles.container} style={{textAlign: "center"}}>
+      Footer
+    </div>
+  );
+};
+
+export default FooterLayout;
