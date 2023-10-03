f8e97e20b45720fb864e64e7e256c1948b8f65f6
KhanhNguyen <nguyen.quoc.khanh.531@gmail.com>
2023-10-03 15:02:01 +0700

    Add data in slice zone prismic

--- src/components/HeaderLayout.tsx
+++ src/components/HeaderLayout.tsx
@@ -1,55 +1,52 @@
-'use client'
-import React, {useEffect, useState} from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import {useLocale} from "next-intl";
-import styles from '@/styles/layout.module.scss'
-import {createClient} from "@/prismicio";
+import { useLocale } from "next-intl";
+import styles from "@/styles/layout.module.scss";
+import { createClient } from "@/prismicio";
 import NavBar from "@/components/NavBar";
 import LocaleSwitcher from "@/components/LocaleSwitcher";
 
 const HeaderLayout = () => {
-  const locale = useLocale()
-  const [settings, setSettings] = useState<any>()
+  const locale = useLocale();
+  const [settings, setSettings] = useState<any>();
 
-  const client = createClient()
+  const client = createClient();
   useEffect(() => {
-    client.getSingle('settings').then((res) => setSettings(res)).catch()
-  }, [])
+    client
+      .getSingle("settings")
+      .then((res) => setSettings(res))
+      .catch();
+  }, []);
 
   return (
-    // <header className={styles.header}>
-    //   <div className={`${styles.header_container} ${styles.container}`}>
-    //     <div>
-    //       <label><Link href={`/${locale}`}>
-    //         {/*<Image className={styles.header_icon} width={50} height={50} src={'/icon-react.png'} alt={'icon'}/>*/}
-    //         {settings ? settings.data.site_title : "Data Mau"}
-    //       </Link></label>
-    //
-    //       <input className={styles.header_input} type={`checkbox`} id={'box'}/>
-    //       <nav className={styles.header_nav}>
-    //         <ul>
-    //           {settings && <NavBar navigation={settings.data.navigation}/>}
-    //         </ul>
-    //       </nav>
-    //     </div>
-    //     <div>
-    //       <div>
-    //         <label className={styles.header_box} htmlFor={'box'}>
-    //           <hr/>
-    //           <hr/>
-    //           <hr/>
-    //         </label>
-    //       </div>
-    //       <LocaleSwitcher/>
-    //     </div>
-    //
-    //   </div>
-    // </header>
-    <header>
-      <div className={`container mx-auto mobile:px-1 ${styles.header}`}>
-        <h1 className="text-3xl font-bold mobile:text-amber-400 tablet:text-amber-50 desktop:text-zinc-800">
-          Hello world!
-        </h1>
+    <header className={styles.header}>
+      <div className={`${styles.header_container} ${styles.container}`}>
+        <div>
+          <label>
+            <Link href={`/${locale}`}>
+              {/*<Image className={styles.header_icon} width={50} height={50} src={'/icon-react.png'} alt={'icon'}/>*/}
+              {settings ? settings.data.site_title : "Data Mau"}
+            </Link>
+          </label>
+
+          <input className={styles.header_input} type={`checkbox`} id={"box"} />
+          <nav className={styles.header_nav}>
+            <ul>
+              {settings && <NavBar navigation={settings.data.navigation} />}
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <div>
+            <label className={styles.header_box} htmlFor={"box"}>
+              <hr />
+              <hr />
+              <hr />
+            </label>
+          </div>
+          <LocaleSwitcher />
+        </div>
       </div>
     </header>
   );
