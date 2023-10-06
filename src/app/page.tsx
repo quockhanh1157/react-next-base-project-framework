336898dabe347666f67f2670a85a79026cef3d48
KhanhNguyen <nguyen.quoc.khanh.531@gmail.com>
2023-10-06 11:03:25 +0700

    Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-import { components } from "@/slices";
+import clsx from "clsx";
+import React from "react";
 
 export default async function Page() {
-  const client = createClient();
-  const page = await client.getSingle("homepage");
-
-  return <SliceZone slices={page.data.slices} components={components} />;
+  return (
+    <div className={clsx("px-4 py-10 mobile:py-14 mobile:px-6 tablet:py-16")}>
+      <div className="mx-auto w-full max-w-6xl">Home</div>
+    </div>
+  );
 }
 
 export async function generateMetadata(): Promise<Metadata> {
