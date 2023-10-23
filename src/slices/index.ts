aa89cd9341ca11b61f2716acc92ba03c3b156976
KhanhNguyen <nguyen.quoc.khanh.531@gmail.com>
2023-10-23 10:38:05 +0700

    fix not merge slice machine to main
    fix not merge slice machine to main

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -6,6 +6,7 @@ export const components = {
   call_to_action: dynamic(() => import("./CallToAction")),
   feature: dynamic(() => import("./Feature")),
   hero: dynamic(() => import("./Hero")),
+  recuitment: dynamic(() => import("./Recuitment")),
   testimonial: dynamic(() => import("./Testimonial")),
   text_width_image: dynamic(() => import("./TextWidthImage")),
 };
